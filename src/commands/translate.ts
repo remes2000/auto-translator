1dff58fcb73a8863351571427e8e19133391ef9e
remes2000 <nieruchalskimichal@gmail.com>
2019-07-25 19:57:07 +0200

    Create config field and read value from it

--- src/commands/translate.ts
+++ src/commands/translate.ts
@@ -1,5 +1,13 @@
 import * as vscode from 'vscode';
 
-export const translate = vscode.commands.registerCommand('extension.translate', () => {
-    vscode.window.showInformationMessage('Translation!!!');
+export const translate = vscode.commands.registerCommand('extension.translate', async () => {
+    const translationKey = await vscode.window.showInputBox({prompt: 'Provide translation key'});
+    if(!translationKey) return;
+    const translationValue = await vscode.window.showInputBox({prompt: 'Provide translation value'});
+    if(!translationValue) return;  
+
+    const config = vscode.workspace.getConfiguration('autoTranslatorExt');
+    const translationFilePath = config['translationFilePath'];
+
+    
 });
